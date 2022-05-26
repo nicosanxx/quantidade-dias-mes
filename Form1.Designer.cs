ab44e497eb305d60a526be62defcd5814895ad64
leonardo.ocruz <leonardo.ocruz@PIR06305W10-1>
2022-05-26 16:47:51 -0300

    quantidade_dias_mes

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace quantidade_dias_mes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bt_case = new System.Windows.Forms.Button();
+            this.bt_if = new System.Windows.Forms.Button();
+            this.cb_mes = new System.Windows.Forms.ComboBox();
+            this.txt_result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(386, 68);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Para demonstrar um exemplo de switch/case, considere um \r\nprograma que informa ao" +
+    " usuário a  \r\nquantidade de dias de um mês a \r\npartir do nome de um mês por ele " +
+    "informado.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Selecione o Mês:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Resultado";
+            // 
+            // bt_case
+            // 
+            this.bt_case.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_case.Location = new System.Drawing.Point(43, 189);
+            this.bt_case.Name = "bt_case";
+            this.bt_case.Size = new System.Drawing.Size(98, 50);
+            this.bt_case.TabIndex = 3;
+            this.bt_case.Text = "Calcular Case";
+            this.bt_case.UseVisualStyleBackColor = true;
+            this.bt_case.Click += new System.EventHandler(this.bt_case_Click);
+            // 
+            // bt_if
+            // 
+            this.bt_if.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_if.Location = new System.Drawing.Point(242, 189);
+            this.bt_if.Name = "bt_if";
+            this.bt_if.Size = new System.Drawing.Size(93, 50);
+            this.bt_if.TabIndex = 4;
+            this.bt_if.Text = "Calcular If";
+            this.bt_if.UseVisualStyleBackColor = true;
+            this.bt_if.Click += new System.EventHandler(this.bt_if_Click);
+            // 
+            // cb_mes
+            // 
+            this.cb_mes.FormattingEnabled = true;
+            this.cb_mes.Location = new System.Drawing.Point(183, 117);
+            this.cb_mes.Name = "cb_mes";
+            this.cb_mes.Size = new System.Drawing.Size(121, 24);
+            this.cb_mes.TabIndex = 5;
+            // 
+            // txt_result
+            // 
+            this.txt_result.Enabled = false;
+            this.txt_result.Location = new System.Drawing.Point(101, 271);
+            this.txt_result.Name = "txt_result";
+            this.txt_result.Size = new System.Drawing.Size(100, 23);
+            this.txt_result.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 313);
+            this.Controls.Add(this.txt_result);
+            this.Controls.Add(this.cb_mes);
+            this.Controls.Add(this.bt_if);
+            this.Controls.Add(this.bt_case);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button bt_case;
+        private System.Windows.Forms.Button bt_if;
+        private System.Windows.Forms.ComboBox cb_mes;
+        private System.Windows.Forms.TextBox txt_result;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quantidade_dias_mes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cb_mes.Items.Add("Janeiro");
+            cb_mes.Items.Add("Fevereiro");
+            cb_mes.Items.Add("Março");
+            cb_mes.Items.Add("Abril");
+            cb_mes.Items.Add("Maio");
+            cb_mes.Items.Add("Junho");
+            cb_mes.Items.Add("Julho");
+            cb_mes.Items.Add("Agosto");
+            cb_mes.Items.Add("Setembro");
+            cb_mes.Items.Add("Outubro");
+            cb_mes.Items.Add("Novembro");
+            cb_mes.Items.Add("Dezembro");
+          
+
+        }
+
+        private void bt_case_Click(object sender, EventArgs e)
+        {
+
+            string mes = (cb_mes.SelectedItem).ToString();
+
+            switch(mes)
+            {
+                case "Janeiro":
+                case "Março":
+                case "Maio":  
+                case "Julho":    
+                case "Agosto":
+                case "Outubro":
+                case "Dezembro":
+                    txt_result.Text = "31 dias";
+                    break;
+
+                case "Fevereiro":
+                    txt_result.Text = "28 ou 29 dias";
+                    break;
+
+                default:
+                    txt_result.Text = "30 dias";
+                    break;
+
+            }
+
+
+        }
+
+        private void bt_if_Click(object sender, EventArgs e)
+        {
+            string mes_if = cb_mes.SelectedItem.ToString();
+
+
+            if (mes_if == "Janeiro" || mes_if == "Março"|| mes_if == "Maio" || mes_if == "Julho" || mes_if == "Agosto" || mes_if == "Outubro" || mes_if == "Dezembro")
+
+            {
+
+                txt_result.Text = "31 dias";
+            }
+            else if (mes_if == "Fevereiro")
+            {
+                txt_result.Text = "28 ou 29 dias";
+
+            }
+            else
+            {
+                txt_result.Text = "30 dias";
+
+
+            }
+
+
+
+
+        }
+    }
+}
